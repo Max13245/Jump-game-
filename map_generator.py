9cdee798f33d824f0f5ea8a0a0973ab5f26852ec
Max <maxrijkers@outlook.com>
2022-07-16 03:22:09 +0200

    deleted unused code

--- map_generator.py
+++ map_generator.py
@@ -19,37 +19,6 @@ class MAP:
         for tile in self.tiles:
             tile.draw_tile(surface)
 
-    def change_tile(self, layer, created_layer):
-        chosen_layer = random.choice(["layer", "created_layer"]) 
-        chosen_direction = random.choice(["hori", "vert"]) 
-
-        if chosen_layer == "layer":
-            if chosen_direction == "vert":
-                (chosen_direction, "down")
-            elif chosen_direction == "hori":
-                pass
-        elif chosen_layer == "created_layer":
-            if chosen_direction == "vert":
-                (chosen_direction, "up")
-            elif chosen_direction == "hori":
-                pass
-
-    def is_posible_jump(self, layer, created_layer):
-        #veranderen, want tile is miss niet altijd 1
-        for indx, tile in enumerate(layer):
-            if tile == 1:
-                if layer[indx - 1] == 0 and indx != 0:
-                    for num in created_layer[indx - 5, indx]:
-                        if num == 1:
-                            return layer, created_layer
-
-                elif layer[indx + 1] == 0 and indx != 15:
-                    for num in created_layer[indx, indx + 5]:
-                        if num == 1:
-                            return layer, created_layer
-            
-            self.change_tile(layer, created_layer)
-
     def create_tiles(self, screen_width, screen_height):
         if len(self.tiles) > 0:
             beneith_layer_y_pos = self.tiles[-1].y
@@ -99,7 +68,7 @@ class MAP:
         max_tiles = 12 / num_platforms #- 1 #doen als verder in map (later -2, niet meer)
         platform_lengths = []
         layer = [0 for _ in range(15)]
-        platform_lengths = [random.randrange(1, max_tiles) for _ in range(num_platforms)] #not debuged yet!
+        platform_lengths = [random.randrange(1, max_tiles) for _ in range(num_platforms)]
 
         if sum(platform_lengths) >= 15:
             layer = [1 for _ in range(15)]
